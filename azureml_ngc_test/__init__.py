81f42601ac18ae2bacc56b82f791ebc4a91a6c52
manuelreyesgomez <manuel.reyesgomez@gmail.com>
2020-07-21 18:51:00 -0700

    __init__.py

---
+++ azureml_ngc_test/__init__.py
@@ -0,0 +1,9 @@
+from . import Azuremlcomputecluster
+from . import ngccontent 
+from . import setup_NGC
+
+#from ._version import get_versions
+
+#__version__ = get_versions()["version"]
+
+#del get_versions
